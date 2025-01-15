5f231f3c954db54188ed8d1fa3190fa1de4af548
Zen <keshav.23fe10cse00589@muj.manipal.edu>
2025-01-15 10:39:41 +0530

    Complete basic js exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let res = (temp - 32) * 5 / 9;
+  res = Math.round(res * 10) / 10;
+  return res;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let res = temp * 9 / 5 + 32;
+  res = Math.round(res * 10) / 10;
+  return res;
 };
 
 // Do not edit below this line

ab87c8cd7112b50a24e36e97b81fdb66dc5808bb
Zen <keshav.23fe10cse00589@muj.manipal.edu>
2025-03-17 12:19:52 +0530

    feat: complete advanced js exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+function findAge(obj) {
+    return (obj.yearOfDeath || new Date().getFullYear()) - obj.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+    return people.reduce((oldest, curr) => {
+        return findAge(oldest) < findAge(curr) ? curr : oldest;
+    });
 };
 
 // Do not edit below this line
